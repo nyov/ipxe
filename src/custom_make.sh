1d7f92923a432fa36a8bf36f963248631e615e53
Pravin Shinde <pravin@black-perl.(none)>
2009-07-27 19:11:55 +0200

    using .bko.tmpl for generating files

--- src/custom_make.sh
+++ src/custom_make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make clean && make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe
-#make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe
+make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe bin/gpxe.lkrn
 
