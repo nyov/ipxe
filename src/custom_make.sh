189e37a9f1998fe3bc63322ee4f77c80ed9ff5ad
Pravin Shinde <pravin@black-perl.(none)>
2009-09-25 11:19:05 +0200

    pulled latest tree and rebased my changes on it.

--- src/custom_make.sh
+++ src/custom_make.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#make clean && make  EMBEDDED_IMAGE=pxeBig.gpxe
+#make  EMBEDDED_IMAGE=pxeBig.gpxe bin/gpxe.lkrn
 make clean && make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe
 make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe bin/gpxe.lkrn
+make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe bin/gpxe.sdsk
+make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe bin/gpxe.pxe
+make  EMBEDDED_IMAGE=pxeDHCP.gpxe,pxeSTATIC.gpxe bin/undionly.kpxe
 
